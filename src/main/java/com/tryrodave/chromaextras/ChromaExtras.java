cd09bbe8e09a45fb8418a233aee04fa29ef17887
tryrodave <73838305+DaveVermeulen@users.noreply.github.com>
2026-07-07 19:32:24 +0300

    Initial setup

---
+++ src/main/java/com/tryrodave/chromaextras/ChromaExtras.java
@@ -0,0 +1,9 @@
+package com.tryrodave.chromaextras;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = ChromaExtras.MODID, version = Tags.VERSION, name = "ChromaExtras", acceptedMinecraftVersions = "[1.7.10]")
+public class ChromaExtras {
+
+    public static final String MODID = "chromaextras";
+}
